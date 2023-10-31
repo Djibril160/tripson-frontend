47069ca38786563eafb2102ed8018d0c36096825
djibril soukouna <djib_cisse_9@hotmail.com>
2023-11-01 00:48:05 +0100

    frontend - mise en place signup

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "719c159c1ecd8f5f"; }
+/******/ 		__webpack_require__.h = function() { return "98f188c3871c4d55"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- components/Home.js
+++ components/Home.js
@@ -1,14 +1,40 @@
 import styles from '../styles/Home.module.css';
+import { useDispatch, useSelector } from 'react-redux';
 import React, { useState } from 'react';
-import { DownloadOutlined, UserOutlined, EyeInvisibleOutlined, EyeTwoTone, PushpinTwoTone } from '@ant-design/icons';
+import { login, logout } from '../reducers/user';
+import {  UserOutlined, PushpinTwoTone } from '@ant-design/icons';
 import { Button, Radio, Space, Divider, Input, Card } from 'antd';
 
 function Home() {
+  const dispatch = useDispatch();
+  const user = useSelector((state) => state.user.value);
+
   const [size, setSize] = useState('large'); // default is 'middle'
   const [passwordVisible, setPasswordVisible] = React.useState(false);
-  const [username, setUsername] = useState(null);
-  const [password, setPassword] = useState(null);
+  const [signupUsername, setSignupUsername] = useState(null);
+  const [signupPassword, setSignupPassword] = useState(null);
   const [city, setCity] = useState(null);
+
+  // function to create a new user
+  const handleSignup = () => {
+    fetch('http://localhost:3000/signup', {
+      method: 'POST',
+      headers: {'Content-Type': 'application/json'},
+      body: JSON.stringify({ username: signupUsername, password: signupPassword, city: city})
+    })
+    .then((response) => response.json())
+    .then((data) => {
+      console.log(data);
+      // if signup successfully, token is sent
+      if (data && data.token) {
+        // stock token in reducer
+        dispatch(login({ token: data.token }));
+        setSignupUsername('');
+        setSignupPassword('');
+        setCity('');
+      }
+    });
+  }; 
 
   return (
     <div>
@@ -27,18 +53,24 @@ function Home() {
               size="large" 
               placeholder="Username" 
               prefix={<UserOutlined />}
+              onChange={(e) => setSignupUsername(e.target.value)}
+              value={signupUsername}
             />
             <Input 
               className='w-100 text-center' 
               size="large" 
               placeholder="City" 
               prefix={<PushpinTwoTone />}
+              onChange={(e) => setCity(e.target.value)}
+              value={city}
             />
             <Space direction="horizontal">
               <Input.Password
                 className='w-100 text-center'
                 size="large"
                 placeholder="Password"
+                onChange={(e) => setSignupPassword(e.target.value)}
+                value={signupPassword}
                 visibilityToggle={{
                   visible: passwordVisible,
                   onVisibleChange: setPasswordVisible,
@@ -57,7 +89,7 @@ function Home() {
             <Button
               className='w-100 text-center'
               type="primary"
-              // onClick={}
+              onClick={ () => handleSignup()}
             >
               Sign up
             </Button>
